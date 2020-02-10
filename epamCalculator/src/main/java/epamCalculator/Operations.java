a15c3feb3d20880e2ea9ac18f12ab5d386a98773
arunnimmoju <47215012+arunnimmoju@users.noreply.github.com>
2020-02-10 23:59:11 +0530

    task 3 done

---
+++ epamCalculator/src/main/java/epamCalculator/Operations.java
@@ -0,0 +1,27 @@
+package epamCalculator;
+
+public class Operations {
+	public float add(float value1,float value2)
+	{
+		return value1+value2;
+	}
+	
+	public float sub(float value1,float value2)
+	{
+		return value1-value2;
+	}
+	
+	public float mul(float value1,float value2)
+	{
+		return value1*value2;
+	}
+	
+	public float div(float value1,float value2) throws Exception 
+	{
+		if(value2==0) throw new Exception("dividing bt zero");
+		{
+		
+		}
+     	return value1/value2;
+	}
+}
